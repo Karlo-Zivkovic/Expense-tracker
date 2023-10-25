a546512f6784d1556b0ad09de7306cf5bc6d54d1
Karlo Živković <karlo.zivkov2@gmail.com>
2023-10-25 15:04:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1>Description</h1>
+One of the starting projects. Using context + reducer to update the state. Separation of components and simple state updates.
+<br />
+
+<h2>Languages and Utilities Used</h2>
+
+- <b>Javascript</b>
+- <b>React</b>
+
+<h2>HOW TO START:</h2>
+
+- <b>npm install</b>
+- <b>npm start</b>
---
+++ src/context/AppReducer.js
@@ -0,0 +1,19 @@
+/*eslint-disable*/
+export default function (state, action) {
+  switch (action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [...state.transactions, action.payload],
+      };
+    default:
+      return state;
+  }
+}
